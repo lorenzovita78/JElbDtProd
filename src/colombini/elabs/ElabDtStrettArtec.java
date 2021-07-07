ad665e8676995288de41e4a9a3b3fc34aa77b779
ggraziani@colombinigroup.com <ggraziani@colombinigroup.com>
2021-07-07 16:28:29 +0200

    Fix ElabDtStrettArtec Controllo Riga <> '---'

--- src/colombini/elabs/ElabDtStrettArtec.java
+++ src/colombini/elabs/ElabDtStrettArtec.java
@@ -199,7 +199,7 @@ public class ElabDtStrettArtec extends ElabClass{
       fR = new FileReader(fileS);
       bfR=new BufferedReader(fR);
       riga = bfR.readLine(); 
-      while(riga!=null && !riga.isEmpty()){
+      while(riga!=null && !riga.isEmpty() && !riga.startsWith("---")){
         DatiStrettoioSqlS dtS=new DatiStrettoioSqlS(dataComm);
         dtS.loadInfo(riga);
         if(dtS.validate())
