2ac62ade39477bd0fae26c179b07d23573e2feb6
ggraziani@colombinigroup.com <ggraziani@colombinigroup.com>
2021-11-10 16:17:14 +0100

    elabdatiprodcomm homag

--- src/colombini/costant/TAPWebCostant.java
+++ src/colombini/costant/TAPWebCostant.java
@@ -28,6 +28,7 @@ public class TAPWebCostant {
   public static final String CDL_STEMAPASCIAR1P4_EDPC = "01086SP";
   public static final String CDL_LSMCARRP4_EDPC = "01030";
   public static final String CDL_LSMCUCINE_EDPC = "01030";
+  public static final String CDL_HOMAGR1P4_EDPC = "01089";
   
   //public static final String CDL_ANTESPECIALI_EDPC = "01008AS";
   public static final String CDL_LAVMISURAIMB_EDPC = "6100IMB";
